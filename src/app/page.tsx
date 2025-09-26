86330791601aad808c5efd002b24d2ade6706b47
alexandre <alexandrenas2003@gmail.com>
2025-09-26 10:42:25 -0300

    PostImageCover component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
+import { PostImageCover } from "@/components/PostImageCover";
+import { PostHeading } from "@/components/PostHeading";
 import { SpinLoader } from "@/components/SpinLoader";
+import { Container } from "@/components/Container";
 import { PostsList } from "@/components/PostsList";
 import { Header } from "@/components/Header";
 
-import { Suspense } from "react";
 import { MainTemplate } from "@/templates/MainTemplate";
-import { Container } from "@/components/Container";
 
-import Link from "next/link";
-import Image from "next/image";
-import { PostHeading } from "@/components/PostHeading";
+import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
@@ -16,16 +15,18 @@ export default async function HomePage() {
       <Container>
         <Header />
         <section className="grid grid-cols-1 gap-8 sm:grid-cols-2 group">
-          <Link href="#" className="w-full h-full overflow-hidden rounded-xl">
-            <Image
-              className="w-full h-full object-cover object-center group-hover:scale-105 transition"
-              src="/images/bryen_0.png"
-              width={1200}
-              height={720}
-              alt="Título do post"
-              priority
-            />
-          </Link>
+          <PostImageCover
+            linkProps={{
+              href: "#",
+            }}
+            imageProps={{
+              src: "/images/bryen_0.png",
+              alt: "Imagem do Post em destaque",
+              width: 1200,
+              height: 720,
+              priority: true,
+            }}
+          />
           <div className="flex flex-col sm:justify-center">
             <time
               className="text-slate-600 text-sm/tight"
@@ -33,7 +34,9 @@ export default async function HomePage() {
             >
               24/09/2025 10:07
             </time>
-            <PostHeading href="#">Olá, novo título do FeaturedPost</PostHeading>
+            <PostHeading href="#" as="h1">
+              Olá, novo título do FeaturedPost
+            </PostHeading>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
             aliquam odio officiis pariatur cupiditate esse voluptatibus vitae
             alias magnam porro! Maxime numquam odit tenetur illum provident,
